e8ae419ce92ad09214e0bd1c69aebbcf3b217415
VictorViPo <87740269+VictorViPo@users.noreply.github.com>
2021-11-13 16:10:00 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1C_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.ReadKey();
+
+
+        }
+    }
+}
