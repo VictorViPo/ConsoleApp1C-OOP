69df6e41782a3bd5a894a21712d9feef95256fed
VictorViPo <87740269+VictorViPo@users.noreply.github.com>
2021-11-18 01:08:46 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,42 +1,71 @@
 ﻿using System;
 
-namespace ConsoleApp1СтарыйЛифт
-
+namespace ConsoleApp6_OOP_C2
 {
-    
-    class Program
+    //internal class Program
+    internal class Account
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
+        { }
+
+
+        public string clientName;
+        private uint accountNumber;
+        private double procent;
+        private double amount;
+
+        public Account(string clientName, uint accountNumber, double procent, double amount)
         {
-            Console.WriteLine("Лифт\nэто задание займет месяц или больше.");
-            Console.ReadKey();
+            this.clientName = clientName;
+            this.accountNumber = accountNumber;
+            this.procent = procent;
+            this.amount = amount;
+        }
 
-            //двери
-            //проверка открытых дверей
-            //проверка закрытых дверей
-            //открытие дверей
-            //закрытие дверей
-            //невозможность закрыть дверь (препятствие)
+        private void Name(string name) // Имя клиента
+        {
+            while (string.IsNullOrWhiteSpace(clientName))
+            {
+                Console.Write("Введите ваше имя: ");
 
-            //кнопки
-            //нажата ли кнопка
-            //вызов лифта с этажа
-            //нажатие кнопки в лифте на этаж ( "1" "2" "3" "..." )
-            //спец кнопоки ("стоп" "открыть" "закрыть" "вызов диспечера")
+                clientName = Console.ReadLine();
 
-            //движение
-            //вверх до "n" этажа
-            //вниз до "n" этажа
-            //остановка на "n" этаже
-            //невозможность движения при открытых дверях
+            }
+            void Number(uint number) // Номер счёта
+            {
+                Console.WriteLine("Ведите номер счёта: ");
+                while (!uint.TryParse(Console.ReadLine(), out number))
 
-            //время
+                {
 
-            //перегруз кабины
+                    accountNumber = number;
 
-            //свет в кабине
+                }
 
+            }
+            void Procent(double proc) // Процент начисления
+            {
+                Console.WriteLine("Процент от начисления составит: ");
+                while (!double.TryParse(Console.ReadLine(), out proc))
+
+                {
+
+                    procent = proc;
+                }
+
+            }
+            void Amount(double summa) // Баланс счёта
+            {
+
+                Console.WriteLine("Баланс счёта составляет: ");
+                while (!double.TryParse(Console.ReadLine(), out summa))
+
+                {
+
+                    amount = summa;
+                }
+
+            }
         }
     }
 }
-
