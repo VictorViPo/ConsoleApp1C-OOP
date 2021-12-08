9479f50b9ac0ce60b9874a580b2a500fd710b020
VictorViPo <87740269+VictorViPo@users.noreply.github.com>
2021-12-09 02:39:26 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,42 +1,166 @@
 ﻿using System;
 
-namespace ConsoleApp1СтарыйЛифт
-
+namespace ConsoleApp2_OOP_C5
 {
-    
-    class Program
+    internal class RationalNumber
     {
-        static void Main(string[] args)
+        private int numerator;    // Числитель
+        private int denominator;  // Знаменатель
+
+        // конструктор
+        public RationalNumber(int numerator, int denominator)
         {
-            Console.WriteLine("Лифт\nэто задание займет месяц или больше.");
-            Console.ReadKey();
+            if (denominator == 0)
+            {
+                throw new DivideByZeroException("В знаменателе не может быть нуля");
+            }
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
 
-            //двери
-            //проверка открытых дверей
-            //проверка закрытых дверей
-            //открытие дверей
-            //закрытие дверей
-            //невозможность закрыть дверь (препятствие)
+        // Вызов первого конструктора 
+        public RationalNumber(int number)
+            : this(number, 1)
+        { }
 
-            //кнопки
-            //нажата ли кнопка
-            //вызов лифта с этажа
-            //нажатие кнопки в лифте на этаж ( "1" "2" "3" "..." )
-            //спец кнопоки ("стоп" "открыть" "закрыть" "вызов диспечера")
+        // Перегрузка оператора для двух дробей
+        public static bool operator ==(RationalNumber a, RationalNumber b)
+        {
+            Object aAsObj = a as Object;
+            Object bAsObj = b as Object;
+            if (aAsObj == null || bAsObj == null)
+            {
+                return aAsObj == bAsObj;
+            }
+            return a.Equals(b);
+        }
 
-            //движение
-            //вверх до "n" этажа
-            //вниз до "n" этажа
-            //остановка на "n" этаже
-            //невозможность движения при открытых дверях
+        // Перегрузка оператора для дроби и числа
+        public static bool operator ==(RationalNumber a, int b)
+        {
+            return a == new RationalNumber(b);
+        }
 
-            //время
+        // Перегрузка оператора для числа и дроби
+        public static bool operator ==(int a, RationalNumber b)
+        {
+            return new RationalNumber(a) == b;
+        }
 
-            //перегруз кабины
+        // Перегрузка оператора для двух дробей
+        public static bool operator !=(RationalNumber a, RationalNumber b)
+        {
+            return !(a == b);
+        }
 
-            //свет в кабине
+        // Перегрузка оператора для дроби и числа
+        public static bool operator !=(RationalNumber a, int b)
+        {
+            return a != new RationalNumber(b);
+        }
 
+        // Перегрузка оператора для числа и дроби
+        public static bool operator !=(int a, RationalNumber b)
+        {
+            return new RationalNumber(a) != b;
+        }
+        // Возвращает наибольший общий делитель
+        private static int getGreatestCommonDivisor(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        // Возвращает сокращенную дробь
+        public RationalNumber Reduce()
+        {
+            RationalNumber result = this;
+            int greatestCommonDivisor = getGreatestCommonDivisor(numerator, denominator);
+            result.numerator /= greatestCommonDivisor;
+            result.denominator /= greatestCommonDivisor;
+            return result;
+        }
+
+        // Метод сравнения двух дробей
+        private int CompareTo(RationalNumber that)
+        {
+            if (Equals(that))
+            {
+                return 0;
+            }
+            RationalNumber a = this.Reduce();
+            RationalNumber b = that.Reduce();
+            if (a.numerator * b.denominator > b.numerator * a.denominator)
+            {
+                return 1;
+            }
+            return -1;
+        }
+
+        // Перегрузка оператора ">" для двух дробей
+        public static bool operator >(RationalNumber a, RationalNumber b)
+        {
+            return a.CompareTo(b) > 0;
+        }
+        // Перегрузка оператора ">" для дроби и числа
+        public static bool operator >(RationalNumber a, int b)
+        {
+            return a > new RationalNumber(b);
+        }
+        // Перегрузка оператора ">" для числа и дроби
+        public static bool operator >(int a, RationalNumber b)
+        {
+            return new RationalNumber(a) > b;
+        }
+        // Перегрузка оператора "<" для двух дробей
+        public static bool operator <(RationalNumber a, RationalNumber b)
+        {
+            return a.CompareTo(b) < 0;
+        }
+        // Перегрузка оператора "<" для дроби и числа
+        public static bool operator <(RationalNumber a, int b)
+        {
+            return a < new RationalNumber(b);
+        }
+        // Перегрузка оператора "<" для числа и дроби
+        public static bool operator <(int a, RationalNumber b)
+        {
+            return new RationalNumber(a) < b;
+        }
+        // Перегрузка оператора ">=" для двух дробей
+        public static bool operator >=(RationalNumber a, RationalNumber b)
+        {
+            return a.CompareTo(b) >= 0;
+        }
+        // Перегрузка оператора ">=" для дроби и числа
+        public static bool operator >=(RationalNumber a, int b)
+        {
+            return a >= new RationalNumber(b);
+        }
+        // Перегрузка оператора ">=" для числа и дроби
+        public static bool operator >=(int a, RationalNumber b)
+        {
+            return new RationalNumber(a) >= b;
+        }
+        // Перегрузка оператора "<=" для двух дробей
+        public static bool operator <=(RationalNumber a, RationalNumber b)
+        {
+            return a.CompareTo(b) <= 0;
+        }
+        // Перегрузка оператора "<=" для дроби и числа
+        public static bool operator <=(RationalNumber a, int b)
+        {
+            return a <= new RationalNumber(b);
+        }
+        // Перегрузка оператора "<=" для числа и дроби
+        public static bool operator <=(int a, RationalNumber b)
+        {
+            return new RationalNumber(a) <= b;
         }
     }
 }
-
