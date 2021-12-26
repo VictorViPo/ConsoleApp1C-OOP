a84676d5b5c0b17590e280512a5cca10c43c29c6
VictorViPo <87740269+VictorViPo@users.noreply.github.com>
2021-12-26 22:44:46 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,42 +1,90 @@
 ﻿using System;
+using System.IO;
 
-namespace ConsoleApp1СтарыйЛифт
-
+namespace ConsoleApp4_OOP_C8
 {
-    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Лифт\nэто задание займет месяц или больше.");
+
+            //вывод дерева
+
+
+            string dirName = "C:\\";
+
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Подкаталоги:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+                //создание папки (TESTDIR) в подкаталоге
+
+            {
+                var directory = new DirectoryInfo(@".");
+                Console.WriteLine(directory.FullName);
+               
+                if (directory.Exists)
+                {
+                    
+                    directory.CreateSubdirectory("SUBDIR");
+
+                   
+                    directory.CreateSubdirectory(@"MyDir\SubMyDir");
+
+                    Console.WriteLine("Директории созданы.");
+                }
+                else
+                {
+                    Console.WriteLine("Директория с именем: {0}  не существует.", directory.FullName);
+                }
+
+                
+                Console.ReadKey();
+            }
+           
+            {
+                string sourceFile = @"C:\Users\Public\public\test.txt";
+                string destinationFile = @"C:\Users\Public\private\test.txt";
+
+               
+                System.IO.File.Move(sourceFile, destinationFile);
+
+                
+                System.IO.Directory.Move(@"C:\Users\Public\public\test\", @"C:\Users\Public\private");
+            }
+        }
+
+        //копируем файлы
+
+        static void Main()
+        {
+            
+            var file = new FileInfo(@"C:\Windows\notepad.exe");
+
+            try
+            {
+                file.CopyTo(@"D:\notepad.exe");
+                Console.WriteLine("Файл успешно скопирован!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
             Console.ReadKey();
-
-            //двери
-            //проверка открытых дверей
-            //проверка закрытых дверей
-            //открытие дверей
-            //закрытие дверей
-            //невозможность закрыть дверь (препятствие)
-
-            //кнопки
-            //нажата ли кнопка
-            //вызов лифта с этажа
-            //нажатие кнопки в лифте на этаж ( "1" "2" "3" "..." )
-            //спец кнопоки ("стоп" "открыть" "закрыть" "вызов диспечера")
-
-            //движение
-            //вверх до "n" этажа
-            //вниз до "n" этажа
-            //остановка на "n" этаже
-            //невозможность движения при открытых дверях
-
-            //время
-
-            //перегруз кабины
-
-            //свет в кабине
-
         }
     }
 }
-
